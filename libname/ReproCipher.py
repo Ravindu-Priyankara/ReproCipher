ea27f29f46beef1cb3038df5b5c558d8ee0d7f05
Ravindu Piyankara <78452012+Ravindu-Priyankara@users.noreply.github.com>
2024-02-29 23:29:13 +0530

    re construct code

--- libname/ReproCipher.py
+++ libname/ReproCipher.py
@@ -1,19 +1,8 @@
-import random
-import string
+import random,string
 
 random.seed(42)
 
 def substitution_cipher(text, key):
-    """
-    Encrypts the input text using a substitution cipher with the given key.
-
-    Args:
-        text (str): The input text to be encrypted.
-        key (int): The encryption key.
-
-    Returns:
-        str: The encrypted text.
-    """
     encrypted_text = ""
     for char in text:
         if char.isalpha():
@@ -23,46 +12,32 @@ def substitution_cipher(text, key):
             encrypted_text += char
     return encrypted_text
 
-def generate_random_text(length):
-    """
-    Generates random text of the specified length.
+def random_cipher_key():
+    key = random.randint(-26, 26)
+    return random_cipher_key() if key == 0 else key
 
-    Args:
-        length (int): The length of the random text to generate.
+def random_text():
+    key = random.randint(0, 6)
+    return ''.join(random.choices(string.ascii_lowercase + string.digits, k=key)), key
 
-    Returns:
-        str: The randomly generated text.
-    """
-    return ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
+def encrypt(text, key, random_text1, random_text2, key1, key2, length):
+    divide = random.randint(0, length - 1)
+    encrypt_text = ''
+    for i, char in enumerate(text):
+        if i == divide:
+            encrypt_text += char + random_text1
+            continue
+        encrypt_text += char
+    return encrypt_text + random_text2 + str(key) + str(key1) + str(key2) + str(length)
 
-def generate_random_key():
-    """
-    Generates a random encryption key.
-
-    Returns:
-        int: The random encryption key.
-    """
-    return random.randint(-26, 26)
-
-def encrypt(text):
-    """
-    Encrypts the input text using a substitution cipher with random parameters.
-
-    Args:
-        text (str): The input text to be encrypted.
-
-    Returns:
-        str: The encrypted text.
-    """
-    key = generate_random_key()
+def main(text):
+    key = random_cipher_key()
     cipher_text = substitution_cipher(text, key)
-    random_text1 = generate_random_text(random.randint(0, 6))
-    random_text2 = generate_random_text(random.randint(0, 6))
     length = len(cipher_text)
-    encrypted_text = cipher_text + random_text1 + str(key) + str(length) + random_text2
-    return encrypted_text
+    random_text1, key1 = random_text()
+    random_text2, key2 = random_text()
+    return (encrypt(cipher_text, key, random_text1, random_text2, key1, key2, length))
 
 if __name__ == "__main__":
-    text = "ravindu"
-    encrypted_text = encrypt(text)
-    print("Encrypted value is:", encrypted_text)
+    print("This algorythm developed by Ravindu Priyankara")
+    main("Ravindu")
