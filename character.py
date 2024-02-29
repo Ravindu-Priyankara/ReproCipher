187a5e9fa0256cb3629c5f8397d6c0c4762225cb
Ravindu Piyankara <78452012+Ravindu-Priyankara@users.noreply.github.com>
2024-02-29 11:41:36 +0530

    ascii value of letters

---
+++ character.py
@@ -0,0 +1,27 @@
+#! /usr/bin env python3
+
+#import string module for getting alphebet
+import string
+
+#getting lowercase letters
+lowercase_letters = list(string.ascii_lowercase)
+
+#getting uppercase letters
+uppercase_letters = list(string.ascii_uppercase)
+
+#show a ascii value of each letter
+def get_ascii(letter):
+    print(f"letter {letter} ascii value is :- "+ str(ord(letter)))
+
+if __name__ == "__main__":
+    #get ascii value of lowercase letters
+    for i in range(26):
+        get_ascii(lowercase_letters[i])
+
+    #add 2 line brakes
+    print("\n\n")
+
+    #get ascii value of uppercase letters
+    for i in range(26):
+        get_ascii(lowercase_letters[i])
+#EOF
